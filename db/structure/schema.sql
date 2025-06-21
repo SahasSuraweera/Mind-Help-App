0aaa37f55f3c0b7957ea760d54dabb0490ac6490
Sahas Suraweera <sahassuraweera11@gmail.com>
2025-06-21 23:11:52 +0530

    Update database files

---
+++ db/structure/schema.sql
@@ -0,0 +1,11 @@
+--1.user service - dbName: user
+
+--2.patient service - dbName: patient
+
+--3.staff service - dbName: staff
+
+--4.appointment service - dbName: appointment
+
+--5.payment service - dbName: payment
+
+
