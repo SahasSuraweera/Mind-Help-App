b05fda3bcab82a62024865ba5752ffee5507c70a
Thilini Piyumika <piyumikat80@gmail.com>
2025-06-29 02:11:28 +0530

    add staff members data

--- db/structure/master_data.sql
+++ db/structure/master_data.sql
@@ -22,6 +22,25 @@ CREATE TABLE records (
 );
 
 --3.staff service - dbName: staff
+--staffmember table
+CREATE TABLE StaffMember (
+    staffId BIGINT PRIMARY KEY AUTO_INCREMENT,
+    userId BIGINT NOT NULL,
+    jobRole VARCHAR(50) NOT NULL,
+    joinedDate DATE,
+    salutation VARCHAR(10),
+    firstName VARCHAR(50) NOT NULL,
+    middleName VARCHAR(50),
+    lastName VARCHAR(50) NOT NULL,
+    nic VARCHAR(20) UNIQUE NOT NULL,
+    gender VARCHAR(10),
+    dateOfBirth DATE,
+    personalEmail VARCHAR(100),
+    staffEmail VARCHAR(100),
+    staffPhone VARCHAR(20),
+    isDeleted BOOLEAN DEFAULT FALSE
+);
+
 
 --4.appointment service - dbName: appointment
 
