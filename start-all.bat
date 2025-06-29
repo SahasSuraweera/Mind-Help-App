b51cf710dd853f080a35a9620215f63b18d611f9
Shalin2110 <shalinheenkenda@gmail.com>
2025-06-29 22:26:28 +0530

    Fixed some stuff

--- start-all.bat
+++ start-all.bat
@@ -3,6 +3,9 @@
 REM Start Patient Service
 start cmd /k "cd patient-service && mvn spring-boot:run"
 
+REM Start Payment Service
+start cmd /k "cd payment-service && mvn spring-boot:run"
+
 REM Start Appointment Service
 start cmd /k "cd appointment-service && mvn spring-boot:run"
 
